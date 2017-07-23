a7baa7c34c90a9e5da4d8161371c9d375640b68e
kazakami <kazakami@kmc.gr.jp>
2017-07-23 18:16:24 +0900

    メッシュを扱うクラスを書き始めた

---
+++ Mesh.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace KzkmEngine
+{
+    class Mesh
+    {
+        class Vertex
+        {
+            float x, y, z;
+        }
+
+        public void LoadFromMQO(string filename)
+        {
+
+        }
+    }
+}
