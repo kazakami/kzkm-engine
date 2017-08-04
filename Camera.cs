2723aafabbe7d709ad17ae3dea1c9c4f98fd3efc
kazakami <kazakami@kmc.gr.jp>
2017-08-05 08:56:53 +0900

    カメラクラスを追加し、WorkerManagerの描画処理の初期化で用いるようにした

---
+++ Camera.cs
@@ -0,0 +1,20 @@
+using OpenTK;
+
+namespace KzkmEngine
+{
+    class Camera : Worker
+    {
+        public Vector3 eye { get; private set; } = new Vector3(10, 10, 10);
+        public Vector3 target { get; private set; } = new Vector3(0, 0, 0);
+        public Vector3 up {get; private set; } = new Vector3(0, 1, 0);
+        public override void Update()
+        {
+
+        }
+
+        public override void Draw()
+        {
+
+        }
+    }
+}
--- WorkerManager.cs
+++ WorkerManager.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
 
 namespace KzkmEngine
 {
     class WorkerManager
     {
+        //Workerを保持する
         private List<Worker> workers = new List<Worker>();
+
+        private Camera camera = new Camera();
 
         public void Update()
         {
@@ -17,6 +22,18 @@ namespace KzkmEngine
         }
         public void Draw()
         {
+            //視点の初期化
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            
+            Matrix4 modelView = Matrix4.LookAt(camera.eye, camera.target, camera.up);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelView);
+ 
+            // Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)800.0 / (float)600.0, 1.0f, 64.0f);
+            // GL.MatrixMode(MatrixMode.Projection);
+            // GL.LoadMatrix(ref projection);
+            Utility.RenderSphere(10, 10);
+
             workers.Sort((a, b) => a.priority - b.priority);
             foreach (var worker in workers)
             {
