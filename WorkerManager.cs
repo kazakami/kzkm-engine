c3f28dcd12d234fe891635fefb7f5c53c617c5a5
kazakami <kazakami@kmc.gr.jp>
2017-09-12 11:14:53 +0900

    WorkerManager内にてCameraクラスの情報を元に視点を設定するようにした。

--- WorkerManager.cs
+++ WorkerManager.cs
@@ -22,16 +22,14 @@ namespace KzkmEngine
         }
         public void Draw()
         {
-            //視点の初期化
+            //バッファ初期化
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            
+         
+            //視点の設定  
             Matrix4 modelView = Matrix4.LookAt(camera.eye, camera.target, camera.up);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref modelView);
  
-            // Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)800.0 / (float)600.0, 1.0f, 64.0f);
-            // GL.MatrixMode(MatrixMode.Projection);
-            // GL.LoadMatrix(ref projection);
             Utility.RenderSphere(10, 10);
 
             workers.Sort((a, b) => a.priority - b.priority);
