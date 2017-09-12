f8a4416d8b2b791a831f68fd3d847c8ff87e3e01
kazakami <kazakami@kmc.gr.jp>
2017-09-12 14:26:08 +0900

    テストを追加できるようにディレクトリ構造を変更

---
+++ src/VBO.cs
@@ -0,0 +1,7 @@
+namespace KzkmEngine
+{
+    class VBO
+    {
+        
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using KzkmEngine;
+
+namespace test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
