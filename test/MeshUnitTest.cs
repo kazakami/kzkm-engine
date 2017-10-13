3d29c00a60dbde0e36eec9a29cc3a81c4ccdf55c
kazakami <kazakami@kmc.gr.jp>
2017-10-14 02:22:30 +0900

    ファイル名変更

--- test/MeshUnitTest.cs
+++ test/MeshUnitTest.cs
@@ -4,7 +4,7 @@ using KzkmEngine;
 
 namespace test
 {
-    public class UnitTest1
+    public class MeshUnitTest
     {
         [Fact(DisplayName = "mqoファイルの読み込みの関数に存在しないファイルを渡すと例外が投げられる")]
         public void MeshTest1()
